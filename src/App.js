662a2be85afdcbe0ad396fb64b209a9973aab626
chaturvedakanksha <chaturvedakanksha@gmail.com>
2020-07-24 16:07:14 +0530

    first commit'

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+import { Cards, Chart, CountryPicker } from './component';  
+import styles from './App.module.css';
+
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className = {styles.container} >
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default App;
